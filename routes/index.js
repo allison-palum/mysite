d16d9ce0883d2575264615f115cffe37f127a722
Allison Palum <allisonpalum@Allisons-MBP-2.fios-router.home>
2020-02-02 23:06:34 -0500

    add layout and photos

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    
+  });
+});
+
+router.get('/photos', function(req, res){
+  res.render('photos', {
+    title: 'Photos'
+  });
+});
+
+router.get('/what-im-listening-to', function(req, res){
+  res.render('what-im-listening-to', {
+    title: 'What I\'m listening to'
+  });
+});
+
+router.get('/resume', function(req, res){
+  res.render('resume', {
+    title: 'Resume'
+  });
 });
 
 module.exports = router;
